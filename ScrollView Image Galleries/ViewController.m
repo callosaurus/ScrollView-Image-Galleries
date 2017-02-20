dcb91cf5df4ebc704d5ca006ea482373be582d07
callosaurus <callumdavies421@gmail.com>
2017-02-20 12:00:53 -0800

    Update paging with three images

--- ScrollView Image Galleries/ViewController.m
+++ ScrollView Image Galleries/ViewController.m
@@ -10,6 +10,11 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (strong, nonatomic) UIImageView *imageView1;
+@property (strong, nonatomic) UIImageView *imageView2;
+@property (strong, nonatomic) UIImageView *imageView3;
+
 @end
 
 @implementation ViewController
@@ -17,6 +22,37 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.view.backgroundColor = [UIColor blackColor];
+    self.scrollView.pagingEnabled = YES;
+    
+    self.imageView1 = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"Lighthouse-in-Field"]];
+    [self.scrollView addSubview:self.imageView1];
+   
+    self.imageView2 = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"Lighthouse-night"]];
+    [self.scrollView addSubview:self.imageView2];
+
+    self.imageView3 = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"Lighthouse-zoomed"]];
+    [self.scrollView addSubview:self.imageView3];
+
+}
+
+-(void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    CGFloat scrollViewWidth = CGRectGetWidth(self.scrollView.frame);
+    CGFloat scrollViewHeight = CGRectGetHeight(self.scrollView.frame);
+    
+    self.imageView1.frame = CGRectMake(scrollViewWidth*0, 0, scrollViewWidth, scrollViewHeight);
+    self.imageView1.contentMode = UIViewContentModeScaleAspectFit;
+    
+    self.imageView2.frame = CGRectMake(scrollViewWidth*1, 0, scrollViewWidth, scrollViewHeight);
+    self.imageView2.contentMode = UIViewContentModeScaleAspectFit;
+    
+    self.imageView3.frame = CGRectMake(scrollViewWidth*2, 0, scrollViewWidth, scrollViewHeight);
+    self.imageView3.contentMode = UIViewContentModeScaleAspectFit;
+
+    self.scrollView.contentSize = CGSizeMake(scrollViewWidth*3, scrollViewHeight);
 }
 
 
